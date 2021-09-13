d4117cc7eae0b9938a02d7730684ef7cb384a32a
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2021-09-13 17:29:22 -0300

    api com strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '29825c24a109df6ed2515109aef23ef7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8fe9dd78-b272-44b0-8048-af607de07208'
+};
